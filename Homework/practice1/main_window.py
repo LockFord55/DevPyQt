5b02c477858a0c62a7edcc8f91d99358f72e2825
LockFord55 <lockford55@gmail.com>
2024-04-22 22:07:05 +0300

    Практическая работа №1. Начало работы над пунктом №4.

---
+++ Homework/practice1/main_window.py
@@ -0,0 +1,8 @@
+from PySide6 import QtWidgets
+
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Основное окно')
+        self.resize(300, 200)
